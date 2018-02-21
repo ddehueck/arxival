189034e835d4373ce7d12c480f21743768631a31
ddehueck <d.dehueck@gmail.com>
2018-02-20 23:52:41 -0500

    Attempted mobile nav fix

--- public/javascripts/nav.js
+++ public/javascripts/nav.js
@@ -1,17 +1,18 @@
-var asideNav = document.getElementsByTagName('aside')[0];
+var asideNavClick = document.getElementById('mobile-nav-click');
+var aside = document.getElementsByTagName('aside')[0];
 var exitBtn = document.getElementById('mobile-exit');
 
 document.onclick = function(e) {
 	if (window.matchMedia("(max-width: 1040px)").matches) {
 		// We're in mobile layout
-		if (asideNav == e.target) {
-			if (!asideNav.classList.contains('mobile-viewable-nav')) {
+		if (asideNavClick == e.target) {
+			if (!aside.classList.contains('mobile-viewable-nav')) {
 				// Clicked on nav
-				asideNav.classList.toggle('mobile-viewable-nav');
+				aside.classList.toggle('mobile-viewable-nav');
 			}
 
 		} else if (exitBtn.contains(e.target)) {
-			asideNav.classList.toggle('mobile-viewable-nav');
+			aside.classList.toggle('mobile-viewable-nav');
 		}
 	}
 };

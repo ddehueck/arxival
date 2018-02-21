2fbf51fb45463a2e121b10067b1e6058305f0dc6
Devin de Hueck <d.dehueck@gmail.com>
2018-02-21 03:26:17 +0000

    Working on server configuration

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var Dotenv = require('dotenv').config()
 
 var index = require('./routes/index');
 var user = require('./routes/user');
@@ -13,17 +14,17 @@ var subjects = require('./routes/subjects');
 var search = require('./routes/search');
 
 var mongoose = require('mongoose');
-	var connection_url = '';
+	var connection_url = 'mongodb://';
 
 	if (process.env.DB_NAME && process.env.DB_USER && process.env.DB_PSWD) {
-		connection_url += 'mongodb://' + process.env.DB_USER + ':';
-		connection_url += process.env.DB_PSWD + '@localhost:27017/';
+		connection_url += process.env.DB_USER + ':';
+		connection_url += process.env.DB_PSWD + '@localhost/';
 		connection_url += process.env.DB_NAME;
 	} else {
-		connection_url += 'mongodb://localhost:27017/arxival-test';
+		connection_url += 'localhost:27017/arxival-test';
 	}
 	console.log(connection_url);
-	mongoose.connect(connection_url);
+	mongoose.connect(connection_url, {auth:{authdb:"admin"}});
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -41,13 +42,14 @@ var sessionSecret = '53qS4aDGS5078hefFD7alI';
 if (process.env.NODE_ENV == 'production') {
 	sessionSecret = process.env.SESSION_SECRET;
 }
+var sess = {
+  secret: sessionSecret,
+  cookie: {maxAge: 5184000},
+  resave: false,
+  saveUninitialized: true,
+}
 
-app.use(session({
-	secret: sessionSecret,
-	cookie: { maxAge: 5184000 },
-	resave: false,
-	saveUninitialized: true,
-}));
+app.use(session(sess));
 
 if (process.env.NODE_ENV === 'production') {
   app.set('trust proxy', 1); // trust first proxy
